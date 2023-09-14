eef59441bd6779037a8c4972cf08d18b66af2608
vart <artuhin@yahoo.com>
2023-09-14 16:47:01 +0200

    Added security config by IP

--- src/main/java/ua/vart/portfolio/config/SecurityConfig.java
+++ src/main/java/ua/vart/portfolio/config/SecurityConfig.java
@@ -1,5 +1,12 @@
 package ua.vart.portfolio.config;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,32 +14,57 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
+import org.springframework.web.filter.GenericFilterBean;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+import java.io.IOException;
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${allowed.urls}")
+    private String[] allowedUrls;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .addFilterBefore(ipFilter(), ChannelProcessingFilter.class)
                 .authorizeHttpRequests((authorizeRequests) ->
                         authorizeRequests
                                 .anyRequest().permitAll()
-
                 )
                 .csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("{noop}password")
+    public GenericFilterBean ipFilter() {
+        return new GenericFilterBean() {
+            @Override
+            public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+                HttpServletRequest httpRequest = (HttpServletRequest) request;
+                String clientIp = httpRequest.getRemoteAddr();
+                if (Arrays.stream(allowedUrls).anyMatch(clientIp::matches)) {
+                    chain.doFilter(request, response);
+                } else {
+                    response.getWriter().write("Access Denied");
+                    ((HttpServletResponse) response).setStatus(403); // Forbidden
+                }
+            }
+        };
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        UserDetails user = User.withUsername("ejhrfgeuhrg")
+                .password(encoder.encode("sefwefhweklfw"))
                 .roles("USER")
                 .build();
         return new InMemoryUserDetailsManager(user);
